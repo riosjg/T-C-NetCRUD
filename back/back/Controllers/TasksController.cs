e57cfa8097ad5bbfc1ebc5b7b59d650b718ffb69
Gextox <gabriel_il@live.com>
2020-07-10 22:48:03 -0300

    Creation of TasksRepository, TasksRepository Interface Get, Post and Delete Task methods.

---
+++ back/back/Controllers/TasksController.cs
@@ -0,0 +1,54 @@
+﻿using back.Repository;
+using Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace back.Controllers
+{
+    public class TasksController : ApiController
+    {
+        private readonly TasksRepository _tasksRepository;
+        private static readonly ILog Log = LogManager.GetLogger<TasksController>();
+        [HttpGet]
+        [Route("~/api/tasks")]
+        public IHttpActionResult GetAllTasks()
+        {
+            try
+            {
+                Log.Debug("TasksController - GetAllTasks - Getting every task");
+                var tasks = _tasksRepository.GetAllTasks();
+                return Ok(tasks);
+            }
+            catch(Exception ex)
+            {
+                Log.Error($"{ex}");
+                return InternalServerError();
+            }
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ back/back/Repository/TasksRepository.cs
@@ -0,0 +1,41 @@
+﻿using back.DAL;
+using back.Models;
+using back.Repository.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebGrease.Activities;
+
+namespace back.Repository
+{
+    public class TasksRepository : ITasksRepository
+    {
+        TCDbContext db = new TCDbContext();
+        public List<Task> GetAllTasks()
+        {
+            var query = db.Tasks.ToList();
+            return query;
+        }
+
+        public Task GetTask(int id)
+        {
+            var query = db.Tasks.Find(id);
+            return query;
+        }
+
+        public void DeleteTask(int id)
+        {
+            db.Tasks.Remove(db.Tasks.Find(id));
+            db.SaveChanges();
+        }
+
+        public void CreateTask(Task task)
+        {
+            db.Tasks.Add(task);
+            db.SaveChanges();
+        }
+
+
+    }
+}
